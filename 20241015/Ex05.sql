5b7ec9915ce2ff234c616245da57c291fddb24ef
Le Phan Tri Duc <87744086+lephantriduc@users.noreply.github.com>
2024-10-15 22:58:42 +0700

    Add a bonus to the last question

--- 20241015/Ex05.sql
+++ 20241015/Ex05.sql
@@ -1,10 +1,13 @@
 SELECT
     l.productLine,
-    SUM(p.quantityInStock) AS totalCount
+    COALESCE(SUM(p.quantityInStock), 0) AS totalCount
 FROM
     productlines AS l
-    LEFT JOIN products AS p ON l.productLine = p.productLine
+    INNER JOIN products AS p ON l.productLine = p.productLine
 GROUP BY
     l.productLine
 ORDER BY
     totalCount DESC
+
+# Extra problem: If there exists a product line with no products => return a total count of 0
+# => use COALESCE
