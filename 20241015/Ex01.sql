e49bbf162858ed61a208ca0fd0f8850ae7dbdcea
Le Phan Tri Duc <87744086+lephantriduc@users.noreply.github.com>
2024-10-16 10:11:38 +0700

    Fix a few problems

--- 20241015/Ex01.sql
+++ 20241015/Ex01.sql
@@ -1,7 +1,7 @@
 SELECT
     employees.employeeNumber                             AS id,
     CONCAT(employees.firstName, ' ', employees.lastName) AS fullName,
-    offices.officeCode
+    offices.addressLine1                                 AS office
 FROM
     employees
     JOIN offices ON employees.officeCode = offices.officeCode
--- 20241015/Ex03.sql
+++ 20241015/Ex03.sql
@@ -1,11 +1,8 @@
 SELECT
     p.productName,
-    o.orderNumber,
-    c.customerName
+    d.orderNumber
 FROM
     products AS p
     LEFT JOIN orderdetails AS d ON p.productCode = d.productCode
-    LEFT JOIN classicmodels.orders o ON d.orderNumber = o.orderNumber
-    LEFT JOIN customers AS c ON o.customerNumber = c.customerNumber
 WHERE
-    customerName IS NULL
+    d.productCode IS NULL
