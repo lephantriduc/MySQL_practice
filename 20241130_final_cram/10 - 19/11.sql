919957c00703ab00c4a2fecad32079ce7211ff4b
Le Phan Tri Duc <87744086+lephantriduc@users.noreply.github.com>
2024-11-30 23:58:31 +0700

    Reformat 11

--- 20241130_final_cram/10 - 19/11.sql
+++ 20241130_final_cram/10 - 19/11.sql
@@ -1,5 +1,7 @@
 -- 11.	List orders made between June 16, 2004 and July 7, 2004 (8)
-select
+SELECT
     o.orderNumber
-from orders o
-where o.orderDate BETWEEN '2004-6-16' and '2004-7-7'
+FROM
+    orders o
+WHERE
+    o.orderDate BETWEEN '2004-6-16' AND '2004-7-7'
