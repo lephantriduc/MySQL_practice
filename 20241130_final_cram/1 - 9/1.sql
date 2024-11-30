04760d7d0d2b1e4e7824e79b43a04b2f5404d9ef
Le Phan Tri Duc <87744086+lephantriduc@users.noreply.github.com>
2024-11-30 23:42:55 +0700

    Solve 1

---
+++ 20241130_final_cram/1 - 9/1.sql
@@ -0,0 +1,34 @@
+-- 1.	List all the data in the classic models database:
+-- a)	Product Lines (7)
+-- b)	Product (110);
+-- c)	Employees (23)
+-- d)	Offices (7)
+-- e)	Customers (122)
+-- f)	Orders (326)
+-- g)	Orderdetails (2996)
+-- h)	Payments (273)
+
+SELECT *
+FROM
+    productlines;
+SELECT *
+FROM
+    products;
+SELECT *
+FROM
+    employees;
+SELECT *
+FROM
+    offices;
+SELECT *
+FROM
+    customers;
+SELECT *
+FROM
+    orders;
+SELECT *
+FROM
+    orderdetails;
+SELECT *
+FROM
+    payments;
